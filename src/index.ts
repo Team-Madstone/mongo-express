d5aefbb673304c8209bcb171ffeb89a8d6cd1dc5
madstone-dev <50011531+madstone-dev@users.noreply.github.com>
2023-01-15 15:05:22 +0900

    :tada: express default setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("tsconfig");
+import express, { Request, Response } from "express";
+
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("hello express");
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
+});
